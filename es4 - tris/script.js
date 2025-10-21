fae2d4c5c0f9dc68c42b5255cb37b84312f4eb31
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2025-10-21 18:53:40 +0200

    refactored

--- es4 - tris/script.js
+++ es4 - tris/script.js
@@ -1,134 +1,188 @@
-const audioPop = new Audio("sounds/pop.flac")
-const audioClave = new Audio("sounds/clave.wav")
-const audioFanfare = new Audio("sounds/fanfare.wav")
-const audioPageflip = new Audio("sounds/pageflip.wav")
+class GiocoTris {
+    constructor(schermoId, messaggioId, restartBtnId) {
+        // load sounds        
+        this.audioPop = new Audio("sounds/pop.flac")
+        this.audioClave = new Audio("sounds/clave.wav")
+        this.audioFanfare = new Audio("sounds/fanfare.wav")
+        this.audioPageflip = new Audio("sounds/pageflip.wav")
 
+        this.messaggio = document.getElementById(messaggioId)
+        this.schermo = document.getElementById(schermoId);
+        this.restartBtn = document.getElementById(restartBtnId)
 
-const messaggio = document.getElementById("messaggio")
-const schermo = document.getElementById("schermo");
-const tiles = [];
+        this.tiles = [];
 
-for (let i = 1; i < 10; i++) {
-    tileObj = {
-        element: document.getElementById('tile'+i),
-        utilizzata: false
-    }
-    tiles.push(tileObj);
-    //tiles[i-1].element.innerHTML = `${i-1}`; // write the number of the cell for DEBUG
-}
-
-
-
-
-let playerSelector = true;
-let giocoAttivo = true;
-
-for (const tile of tiles) {
-    tile.element.addEventListener("click", (event) => {
-            if (giocoAttivo) {
-                if (tile.utilizzata == true) {
-                    return;
-                }
-                if (playerSelector) {
-                    event.currentTarget.innerHTML = "X";
-                }
-                else {
-                    event.currentTarget.innerHTML = "O";
-                }
-                audioPop.currentTime = 0;
-                audioPop.play();
-                event.currentTarget.style.color = "#F2E9E4";
-                tile.utilizzata = true;          // flagga quella tile come utilizzata
-                tile.element.style.boxShadow = "0 0 0px #fff";   // disattiva l'animazione al click
-                
-                if (boardPiena() || vittoria()) {  // ferma il gioco se ho vinto o non ci sono piu caselle vuote
-                    fermaGioco();
-                    giocoAttivo = false;
-                    return;
-                }
-                playerSelector = !playerSelector;   
+        for (let i = 1; i < 10; i++) {
+            let tileObj = {
+                element: document.getElementById('tile'+i),
+                utilizzata: false
             }
+            this.tiles.push(tileObj);
+            //this.tiles[i-1].element.innerHTML = `${i-1}`; // write the number of the cell for DEBUG
         }
-    )
-}
 
-restartBtn.addEventListener("click", resetGioco);
-
-
-function boardPiena() {       
-    if (tiles.every(t => t.utilizzata)) {
-        return true;
-    }
-    return false;
-}
-
-function fermaGioco() {
-    // check stato tiles
-    if (vittoria()) {
-        // il giocatore che ha appena mosso ha vinto
-        let activePlayer = "X";
-        if (!playerSelector) activePlayer = "O";
-        messaggio.innerHTML = `${activePlayer} WON`;
-        audioFanfare.currentTime = 0;
-        audioFanfare.play();
-
-    } 
-    else {
-        messaggio.innerHTML = `DRAW`;
-        audioClave.currentTime = 0;
-        audioClave.play();
     }
 
+    inizializza() { 
+        for (const tile of this.tiles) {
+            tile.element.innerHTML = "";
+            tile.element.style.color = "";
+            tile.element.style.boxShadow = "";
+            tile.utilizzata = false; 
+        }
+
+        this.messaggio.innerHTML = "";
+        this.restartBtn.style.display = "none";
+        this.giocoAttivo = true;
+        this.playerSelector = true;      
+    }
+
+    giocaPartita() {
+        this.inizializza(); // configura tutti i parametri per l'inizio della partita
+
+        for (const tile of this.tiles) {
+            tile.element.addEventListener("click", (event) => this.giocaTurno(tile, event))
+        }
+
+        this.restartBtn.addEventListener("click", () => this.resetPartita());
+    }
+
+    giocaTurno(tile, event) {
+        if (!this.giocoAttivo || tile.utilizzata) return;
     
-    // mostra pulsante per rigiocare
-    restartBtn.style.display = 'inline-block';
-}
+        if (this.playerSelector) {
+            event.currentTarget.innerHTML = "X";
+        }
+        else {
+            event.currentTarget.innerHTML = "O";
+        }
 
+        this.audioPop.currentTime = 0;
+        this.audioPop.play();
+        event.currentTarget.style.color = "#F2E9E4";
+        tile.utilizzata = true;          // flagga quella tile come utilizzata
+        tile.element.style.boxShadow = "none";   // disattiva l'animazione al click
+        
+        if (this.boardPiena() || this.vittoria()) {  // ferma il gioco se ho vinto o non ci sono piu caselle vuote
+            this.fermaGioco();
+            this.giocoAttivo = false;
+        }
+        else {
+            this.playerSelector = !this.playerSelector;   
+        }
 
-function vittoria() {
-    const winConditions = [];
-
-    function pushWinCondition(i, j, k) {
-        let condition = (tiles[i].element.innerHTML == tiles[k].element.innerHTML)
-                         && (tiles[k].element.innerHTML == tiles[j].element.innerHTML) 
-                         && (tiles[i].element.innerHTML != '')
-
-        winConditions.push(condition)
-    }
-
-    pushWinCondition(0,1,2);
-    pushWinCondition(3,4,5);
-    pushWinCondition(6,7,8);
-    pushWinCondition(0,3,6);
-    pushWinCondition(1,4,7);
-    pushWinCondition(2,5,8);
-    pushWinCondition(0,4,8);
-    pushWinCondition(2,4,6);    
-    
-    if (winConditions.some(condition => condition)) {
-        return true;
-    }
-    return false;
-
-}
-
-
-function resetGioco() {
-    audioPop.currentTime = 0;
-    audioPop.playbackRate = 0.2;
-    audioPop.play();
-    audioPop.playbackRate = 1;  // ripristino il playback rate normale
-    audioPageflip.currentTime = 0.6;
-    audioPageflip.play();
-    for (const tile of tiles) {
-        tile.element.innerHTML = "";
-        tile.element.style.color = "";
-        tile.element.style.boxShadow = "";
-        tile.utilizzata = false; 
     }
     
-    messaggio.innerHTML = "";
-    restartBtn.style.display = "none";
-    giocoAttivo = true;
-    playerSelector = true;
+    fermaGioco() {
+        // check stato tiles
+        if (this.vittoria()) {
+            // il giocatore che ha appena mosso ha vinto
+            let activePlayer = "X";
+            if (!this.playerSelector) activePlayer = "O";
+            this.messaggio.innerHTML = `${activePlayer} WON`;
+            this.audioFanfare.currentTime = 0;
+            this.audioFanfare.play();
+        } 
+        else {
+            this.messaggio.innerHTML = `DRAW`;
+            this.audioClave.currentTime = 0;
+            this.audioClave.play();
+        }
+
+        // disattiva il behaviour di tutte le tile (segnandole utilizzate e disattivando animazione) 
+        for (let tile of this.tiles) {
+            tile.utilizzata = true;  
+            tile.element.style.boxShadow = "none";   // disattiva l'animazione al click
+        }
+        // mostra pulsante per rigiocare
+        this.restartBtn.style.display = 'inline-block';
+    }
+
+
+    boardPiena() {       
+        if (this.tiles.every(t => t.utilizzata)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    vittoria() {
+        const winConditions = [];
+
+        function pushWinCondition(i, j, k, tiles) {
+            let condition = (tiles[i].element.innerHTML == tiles[k].element.innerHTML)
+                            && (tiles[k].element.innerHTML == tiles[j].element.innerHTML) 
+                            && (tiles[i].element.innerHTML != '')
+
+            winConditions.push(condition)
+            if (condition) {
+                tiles[i].element.style.color = "#f2de05ff";
+                tiles[j].element.style.color = "#f2de05ff";
+                tiles[k].element.style.color = "#f2de05ff";
+            }
+           
+        }
+
+        pushWinCondition(0,1,2, this.tiles);
+        pushWinCondition(3,4,5, this.tiles);
+        pushWinCondition(6,7,8, this.tiles);
+        pushWinCondition(0,3,6, this.tiles);
+        pushWinCondition(1,4,7, this.tiles);
+        pushWinCondition(2,5,8, this.tiles);
+        pushWinCondition(0,4,8, this.tiles);
+        pushWinCondition(2,4,6, this.tiles);    
+        
+        if (winConditions.some(condition => condition)) {
+            return true;
+        }
+        return false;
+    }
+
+    resetPartita() {
+        // funzione per quando hai già giocato e premi il pulsante per giocare ancora
+        this.audioPop.currentTime = 0;
+        this.audioPop.playbackRate = 0.2;
+        this.audioPop.play();
+        this.audioPop.playbackRate = 1;  // ripristino il playback rate normale
+        this.audioPageflip.currentTime = 0.6;
+        this.audioPageflip.play();
+        this.giocaPartita(); 
+    }
 }
+
+
+gioco = new GiocoTris("schermo", "messaggio", "restartBtn");
+gioco.giocaPartita();
+
+
+
+
+/////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
